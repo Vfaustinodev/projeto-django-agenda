ddbb361929b005f9c6b13fbc475dc3c778d9f496
Virginio <f.v.nio.oliveira@hotmail.com>
2023-11-21 16:30:16 -0300

    Atualizações com MakeMigrations e Migrate

--- contact/admin.py
+++ contact/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from contact.models import Contact
 
 # Register your models here.
+@admin.register(Contact)
+class ContactAdmin(admin.ModelAdmin):
+    list_display = ('id', 'first_name', 'last_name', 'email', 'phone')
+    ordering = 'id', 
+    # list_filter = 'created_date',
+    search_fields = 'id', 'first_name', 'last_name',
+    list_per_page = 10 #Limita quantidas de contatos por página
+    list_max_show_all = 200 #Limita a quantidade de contatos que podem ser mostrados no "Mostrar Tudo"
+    # list_editable = 'first_name', 'last_name', #Não é muito bom a utilização e não pode ser utilizado com list_display_links
+    list_display_links = 'id', 'first_name',
